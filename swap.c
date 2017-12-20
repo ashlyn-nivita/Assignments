c95a40fd205e88e81be43b55606d05165bff7b8f
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2017-12-20 18:58:51 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,4 +1,4 @@
-    #include <stdio.h>
+#include <stdio.h>
     int main(void)
     {
     char str[30];
@@ -6,12 +6,24 @@
     int i,len;
     gets(str);
     len=strlen(str);
+    if(len%2==0)
+    {
     for(i=0;i<len;i+=2)
     {
     temp=str[i];
     str[i]=str[i+1];
     str[i+1]=temp;
     }
-    printf("%s",str);
     }
-     
+    else
+    {
+   for(i=0;i<len-1;i+=2)
+    {
+    temp=str[i+1];
+    str[i+1]=str[i];
+    str[i]=temp;
+    }
+    }
+    printf("%s",str);
+    return 0;
+    }
